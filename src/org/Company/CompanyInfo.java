89e2cc2410c5607663bb8abbc86d166d9b175bc5
Latha-Saravanan <lathasaravanan1294@gmail.com>
2022-08-18 12:44:08 +0530

    firstgit

---
+++ src/org/Company/CompanyInfo.java
@@ -0,0 +1,23 @@
+package org.Company;
+
+public class CompanyInfo {
+	private void companyName() {
+	System.out.println("Company Name is Greens Tech");
+	// TODO Auto-generated method stub
+
+	}private void companyId() {
+		System.out.println("Company Id is GT100");// TODO Auto-generated method stub
+
+	}private void companyAddress() {
+		// TODO Auto-generated method stub
+System.out.println("Address is Greens Technologies, Thoraipakkam, OMr, chn-97");
+	}
+	public static void main(String[] args) {
+		CompanyInfo CI=new CompanyInfo();
+		CI.companyName();
+		CI.companyId();
+		CI.companyAddress();
+	}
+	
+
+}
